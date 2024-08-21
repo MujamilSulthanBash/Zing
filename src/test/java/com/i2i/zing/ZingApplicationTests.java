3e5016e2ac5a787e72b755b025bca9eae2ee4c65
MujamilSulthanBasha <mujamil.sulthanbasha@ideas2it.com>
2024-08-21 15:50:12 +0530

    Feat: Zing project added

---
+++ src/test/java/com/i2i/zing/ZingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i2i.zing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

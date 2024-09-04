17b7307894ec2cdcc16dcd89a63e039cf97a4b5d
saiprasathi2i <saiprasath.munusamy@ideas2it.com>
2024-09-04 11:06:10 +0530

    Feat : tried test method for addorder

--- src/test/java/com/i2i/zing/service/OrderServiceImplTest.java
+++ src/test/java/com/i2i/zing/service/OrderServiceImplTest.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.i2i.zing.common.APIResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
@@ -21,6 +24,7 @@ import com.i2i.zing.model.*;
 import com.i2i.zing.repository.OrderRepository;
 import com.i2i.zing.service.impl.EmailSenderService;
 import com.i2i.zing.service.impl.OrderServiceImpl;
+import org.springframework.http.HttpStatus;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceImplTest {
@@ -51,7 +55,7 @@ public class OrderServiceImplTest {
 
     @BeforeEach
     public void setUp() {
-        cartId = "1c";
+        cartId = "2c";
         cart = Cart.builder()
                 .cartId(cartId)
                 .cartItems(Set.of(CartItem.builder()
@@ -59,9 +63,13 @@ public class OrderServiceImplTest {
                         .cart(cart)
                         .item(Item.builder()
                                 .itemId("1i").build())
-                        .totalPrice(12.0)
+                        .totalPrice(120.0)
                         .quantity(2)
                         .build()))
+                .customer(Customer.builder()
+                        .user(User.builder()
+                                .emailId("email")
+                                .build()).build())
                 .build();
         Set<CartItem> cartItems = new HashSet<>();
         cartItem = CartItem.builder()
@@ -69,7 +77,7 @@ public class OrderServiceImplTest {
                 .cart(cart)
                 .item(Item.builder()
                         .itemId("1i").build())
-                .totalPrice(12.0)
+                .totalPrice(120.0)
                 .quantity(2)
                 .build();
         cartItems.add(cartItem);
@@ -88,5 +96,25 @@ public class OrderServiceImplTest {
                 .darkstore(DarkStore.builder().darkStoreId("1ds").build())
                 .item(Item.builder().itemId("1i").build())
                 .quantity(10).build();
+    }
+
+    @Test
+    public void testAddOrder() {
+        Order order = Order.builder()
+                .orderId("1o")
+                .paymentStatus(PaymentStatus.PAID)
+                .paymentMethod(PaymentMethod.UPI)
+                .cart(cart)
+                .build();
+        when(orderRepository.findByIsDeletedFalse()).thenReturn(List.of(order));
+        when(cartService.getCartAsModel("1c")).thenReturn(cart);
+        when(stockService.getStockByItemId("1i")).thenReturn(stock);
+        orderAssignService.addOrderAssign(order);
+        stockService.reduceStocks(cart.getCartItems());
+        emailSenderService.sendEmail(cart.getCustomer().getUser().getEmailId(), "subject", "body");
+        cartService.addCart(order.getCart().getCustomer());
+        APIResponse apiResponse = orderServiceImpl.addOrder(orderDto);
+        assertEquals(HttpStatus.CREATED.value(), apiResponse.getStatus());
+        assertThat(apiResponse.getData()).isNotNull();
     }
 }
